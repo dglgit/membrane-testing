5bfedd186b85bb4f6a01f12aad0994e76e094f91
Douglas <protonelectron00@gmail.com>
2025-04-07 00:05:22 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# membrane-testing
+
+
+This is a repository of molecular dynamics experiments involving simulating lipid membranes in different solvents. I wanted to see if ethanol would rip the membrane apart. 
+
+Note that trajectory (.dcd) files are omitted because they are too big
---
+++ membraneMD_water.py
@@ -0,0 +1,116 @@
+from openmm.app import *
+from openmm import *
+from openmm.unit import *
+from openff.toolkit.topology import Molecule
+from openff.toolkit.topology import Topology as FFTopology
+from openmmforcefields.generators import GAFFTemplateGenerator
+import sys
+import random
+import numpy as np
+pathRoot="water2_less"
+# Load the membrane and alcohol system
+print("Loading initial structure...")
+#pdb = PDBFile('empty.pdb')  # Input file with initial membrane structure
+forcefield = ForceField('amber14-all.xml', 'amber14/tip3p.xml','amber14/lipid17.xml')
+
+# Add alcohol molecules (e.g., ethanol)
+ethanol = Molecule.from_smiles('CCO')  # Ethanol SMILES
+ethanol.generate_conformers(n_conformers=1)
+ethanol.assign_partial_charges(partial_charge_method='gasteiger')
+ethanol_topology = ethanol.to_topology().to_openmm()
+ethanol_positions = ethanol.conformers[0].to_openmm()
+
+
+# Add GAFF parameters for ethanol
+gaff_template_generator = GAFFTemplateGenerator(molecules=[ethanol])
+forcefield.registerTemplateGenerator(gaff_template_generator.generator)
+
+# Build the system with a POPC membrane
+print("Building the system...")
+modeller = Modeller(ethanol_topology, ethanol_positions)
+modeller.topology.setUnitCellDimensions([3,3,15]*nanometers)
+print("System dimensions:", modeller.topology.getUnitCellDimensions())
+modeller.addMembrane(forcefield, lipidType='POPC', minimumPadding=1*nanometer, membraneCenterZ=0.0*nanometer)
+#modeller.deleteWater()
+modeller.addSolvent(forcefield, numAdded=726)
+positions=modeller.positions
+def get2DRad(v):
+    return float(v[0]**2+v[1]**2)**0.5
+#for residue in modeller.topology.residues():
+#    if residue.name =='UNK':
+#        print('deleting first ethanol')
+#        modeller.delete([residue])
+#        continue
+#    for atom in residue.atoms():
+#        pos=positions[atom.index]
+#        if abs(pos[0])>1*nanometer or abs(pos[1])>1*nanometer:
+#            modeller.delete([residue])
+#            break
+# Add solvent (water) and ethanol molecules
+#modeller.addSolvent(forcefield, model='tip3p', numAdded=30)
+#count=0
+#ethanol_positions_list=[]
+#start=-2.1
+#stop=2.1
+#step=0.4
+#for side in [-7,-5.5,-4,4,5.5,7]:
+#    for i in np.arange(start,stop,step):
+#        for j in np.arange(start,stop,step):
+#            modeller.add(ethanol_topology,ethanol_positions+Vec3(i,j,side)*nanometers)
+#            print(count)
+            #ethanol_positions_list.append(ethanol_positions+Vec3(i,j,side)*nanometer)
+#            count+=1
+#            pass
+#print('done appending')
+#ethanol_topologies=[ethanol_topology]*count
+#off_topologies=FFTopology.from_openmm(modeller.topology, unique_molecules=[ethanol])
+#off_topologies.add_molecules([ethanol]*count)
+
+#modeller.topology=off_topologes.to_openmm()
+#modeller.positions.extend(ethanol_positions_list)
+#modeller.add(ethanol_topologies,ethanol_positions_list)
+#print(f'added {count} ethanols')
+
+# Create the system
+print("Creating the system...")
+system = forcefield.createSystem(
+    modeller.topology,
+    nonbondedMethod=PME,
+    nonbondedCutoff=1.0*nanometer,
+    constraints=HBonds,
+)
+
+# Add a barostat for pressure control (NPT ensemble)
+system.addForce(MonteCarloBarostat(1*atmosphere, 300*kelvin))
+
+# Set up integrator and simulation
+integrator = LangevinIntegrator(300*kelvin, 1/picosecond, 0.001*picoseconds)
+simulation = Simulation(modeller.topology, system, integrator)
+
+# Set initial positions and velocities
+simulation.context.setPositions(modeller.positions)
+simulation.context.setVelocitiesToTemperature(300*kelvin)
+
+# Minimize energy
+print("Minimizing energy...")
+simulation.minimizeEnergy(tolerance=3)
+
+# Equilibrate the system
+with open("init_structure_"+pathRoot+'.pdb','w') as pdbout:
+    PDBFile.writeFile(simulation.topology, simulation.context.getState(getPositions=True).getPositions(), file=pdbout, keepIds=True)
+print("Equilibrating...")
+
+simulation.reporters.append(StateDataReporter(sys.stdout, 1000, step=True, potentialEnergy=True, temperature=True))
+simulation.reporters.append(DCDReporter(f'trajectory_{pathRoot}.dcd', 1000, enforcePeriodicBox=True))
+simulation.step(50000)  # Equilibration for 100 ps
+
+# Production simulation
+print("Running production simulation...")
+simulation.step(1000000)  # Run for ~1 ns
+
+# Save final structure
+with open(f"final_structure_{pathRoot}.pdb", "w") as output:
+    PDBFile.writeFile(simulation.topology, simulation.context.getState(getPositions=True).getPositions(), output)
+
+print("Simulation complete!")
+
